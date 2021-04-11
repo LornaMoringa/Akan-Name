866e319a8fc150875e81dda656a4016f903c6eac
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-04-11 19:47:05 +0300

    update js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let male_names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let female_names = ["Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let gender;
